137b7d17cfd47648d3f088423b34d38b85b2a9f6
evatf01 <evatf01@gmail.com>
2024-02-19 12:19:45 +0100

    Revert "descargar varias entradas"
    This reverts commit cabc366058acabf393be4595aa3b6eee31948fab.

--- src/main/java/com/basketballticketsproject/basketballticketsproject/controller/TicketController.java
+++ src/main/java/com/basketballticketsproject/basketballticketsproject/controller/TicketController.java
@@ -1,39 +1,37 @@
 package com.basketballticketsproject.basketballticketsproject.controller;
 
-import com.aspose.pdf.internal.imaging.internal.Exceptions.IO.FileNotFoundException;
 import com.basketballticketsproject.basketballticketsproject.dao.Pdf;
-import com.basketballticketsproject.basketballticketsproject.entity.Partido;
 import com.basketballticketsproject.basketballticketsproject.entity.Ticket;
 import com.basketballticketsproject.basketballticketsproject.service.FileStorageService;
 import com.basketballticketsproject.basketballticketsproject.service.TicketService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.HttpHeaders;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
+
+import static com.basketballticketsproject.basketballticketsproject.utils.Constants.DATE_FORMATTER;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/cbgranada-api/v1")
 @Slf4j
-@ControllerAdvice
 public class TicketController {
 
     @Autowired
@@ -51,82 +49,52 @@ public class TicketController {
 
 
     //metodo para añadir un partido con el formulario del front
-    // @PostMapping("/crearPartidoConEntradas")
-    // public Long uploadFile(@RequestBody Pdf pdf) throws IOException {
-    //     return 0L;//fileStorageService.storeFile(fileStorageService.getFileBase(pdf.getFile1(), pdf.getFile2()), pdf.getTituloPartido(), pdf.getFechaPartido(), pdf.getFechaPublicacion());
-    // }
+    @PostMapping("/crearPartidoConEntradas")
+    public Long uploadFile(@RequestBody Pdf pdf) throws IOException {
+        return fileStorageService.storeFile(fileStorageService.getFileBase(pdf.getFile()), pdf.getTituloPartido(), pdf.getFechaPartido());
 
-
-
-    @GetMapping("/descargarEntrada/{userId}/{partidoId}")
-    public ResponseEntity< byte[]> getTicketPdf(@PathVariable Long userId, @PathVariable Long partidoId) {
-        ResponseEntity< byte[]> response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        byte[] inputStreamResource = ticketService.getTicketPdf(userId, partidoId);
-
-        if(inputStreamResource != null) {
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_PDF);
-            response = new ResponseEntity<>(inputStreamResource, headers, HttpStatus.OK);
-        }
-
-        return response;
     }
 
 
-    @GetMapping("/descargarEntradasAdicionales/{userId}/{partidoId}/{numEntradas}")
-    public ResponseEntity < byte[] > getTicketsAdicionalesPdf(@PathVariable Long userId, @PathVariable Long partidoId,
-                                                                        @PathVariable int numEntradas) {
-        ResponseEntity< byte[]> response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        byte[] inputStreamResource = ticketService.getTicketsAdicionalesPdf(userId, partidoId, numEntradas);
+    //metodo para añadir un partido, junto con su pdf de entradas DESDE POSTMAN
+    @PostMapping("/subirPdf/{nombrePartido}")
+    public Long uploadFile(@RequestParam("file") MultipartFile file, @PathVariable String nombrePartido,
+                          @RequestParam String fechaPartido) throws IOException {
+        final File convFile =  new File(Objects.requireNonNull(file.getOriginalFilename()));
+        return  fileStorageService.storeFile(convFile, nombrePartido, fechaPartido);
 
-        if(inputStreamResource != null) {
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_PDF);
-            response = new ResponseEntity<>(inputStreamResource, headers, HttpStatus.OK);
-        }
-
-        return response;
     }
-
-
-
 
     //enviar entrada al usuario
-    // @GetMapping("/enviarEntrada/{userID}/{partidoId}")
-    // public ResponseEntity<String> enviarEntrada(@PathVariable Long userID, @PathVariable Long partidoId) throws UnsupportedEncodingException {
-    //     final String entrada = ticketService.enviarEntrada(userID, partidoId);
-    //     return new ResponseEntity<>(entrada, HttpStatus.OK);
-    //    }
-
-    /*
-    @GetMapping("/enviarEntrada/{userID}/{partidoId}/{numEntradas}")
-    public ResponseEntity<List<String>> enviarEntrada(@PathVariable Long userID, @PathVariable Long partidoId,
-                                                      @PathVariable int numEntradas) {
-        final List<String> entrada = ticketService.enviarEntrada(userID, partidoId, numEntradas);
+    @GetMapping("/enviarEntrada/{userID}/{partidoId}")
+    public ResponseEntity<String> enviarEntrada(@PathVariable Long userID, @PathVariable Long partidoId) throws UnsupportedEncodingException {
+        final String entrada = ticketService.enviarEntrada(userID, partidoId);
         return new ResponseEntity<>(entrada, HttpStatus.OK);
        }
 
 
-     */
-
     //metodo para obtener una entrada con su nombre
-    // @GetMapping("/getEntrada/{nombreEntrada}")
-    // public ResponseEntity<byte[]> getImage(@PathVariable String nombreEntrada) throws UnsupportedEncodingException {
-    //     byte[] imageData = fileStorageService.getFileByNumber(nombreEntrada);
+    @GetMapping("/getEntrada/{nombreEntrada}")
+    public ResponseEntity<byte[]> getImage(@PathVariable String nombreEntrada) throws UnsupportedEncodingException {
+        byte[] imageData = fileStorageService.getFileByNumber(nombreEntrada);
 
-    //     return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.valueOf("application/pdf")).body(imageData);
-    // }
+        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.valueOf("application/pdf")).body(imageData);
+    }
 
 
-    @GetMapping("/entradasSobrantes/{idPartido}")
-    public ResponseEntity<Integer> entradasSobrantes(@PathVariable Long idPartido) {
-        int numEntradas = ticketService.contadorEntradasRestantes(idPartido);
-        return new ResponseEntity<>(numEntradas, HttpStatus.OK);
+/*
+    @GetMapping("/entradasSobrantes/{fecha}")
+    public ResponseEntity<byte[]> entradasSobrantes(@PathVariable String fecha) throws IOException {
+        byte[] bytes = sorteoService.obtenerEntradasSobrantes(fecha);
+        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.valueOf("application/pdf")).body(bytes);
 
     }
 
 
-    @GetMapping("/getEntradasNoAsignadas/{id}")
+ */
+
+
+    @GetMapping("/getEntradasNoAsignadas/{fecha}")
     public List<Ticket> getEntradasNoAsignadas(@PathVariable Long id) {
         return  ticketService.getEntradasNoAsignadas(id);
     }
--- src/main/java/com/basketballticketsproject/basketballticketsproject/scheduler/RecontarEntradasScheduler.java
+++ src/main/java/com/basketballticketsproject/basketballticketsproject/scheduler/RecontarEntradasScheduler.java
@@ -2,19 +2,13 @@ package com.basketballticketsproject.basketballticketsproject.scheduler;
 
 import com.basketballticketsproject.basketballticketsproject.entity.Partido;
 import com.basketballticketsproject.basketballticketsproject.repo.PartidoRepo;
-import com.basketballticketsproject.basketballticketsproject.utils.EnviarEmail;
-import jakarta.mail.MessagingException;
-import org.apache.commons.collections4.CollectionUtils;
+import com.basketballticketsproject.basketballticketsproject.repo.UsuarioRepo;
+import com.basketballticketsproject.basketballticketsproject.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Set;
-
-import static com.basketballticketsproject.basketballticketsproject.utils.Constants.DATE_FORMATTER;
+import java.util.List;
 
 @Configuration
 @EnableScheduling
@@ -23,25 +17,27 @@ public class RecontarEntradasScheduler {
     @Autowired
     private PartidoRepo partidoRepo;
 
+
     @Autowired
-    private EnviarEmail enviarEmail;
+    private TicketService sorteoService;
 
-    //@Scheduled(cron = "0 0 13 ? * 4 ")
-    //@Scheduled(cron = " 0 23 10 * * ?")
-    public void enviarCorreo() throws MessagingException {
-        LocalDateTime date = LocalDateTime.now();
-        DateTimeFormatter formatters = DateTimeFormatter.ofPattern(DATE_FORMATTER);
-        String text = date.format(formatters);
-        LocalDateTime parsedDate = LocalDateTime.parse(text, formatters);
-        Set<Partido> partidos = partidoRepo.findPartidosDesdeFechaActual(parsedDate);
+    @Autowired
+    private UsuarioRepo usuarioRepo;
 
-        if (!CollectionUtils.isEmpty(partidos)) {
-            enviarEmail.enviarEmailEntrada(partidos);
+    //@Scheduled(cron = "0 0 12 ? * 4 ")
+    public void enviarCorreo() {
+        //List<Partido> fechasSortAsc = partidoRepo.getFechasSortAsc();
+        /*
+        if (!CollectionUtils.isEmpty(fechasSortAsc)) {
+            Partido partido = fechasSortAsc.get(0);
+            System.out.println(partido.getFechaPartido());
+            Set<Usuario> usuariosSorteo = sorteoService.getUsuariosSorteo(partido.getFechaPartido());
+            usuariosSorteo.forEach(usuario -> {
+
+            });
         }
 
+         */
     }
 
-
 }
-
-
