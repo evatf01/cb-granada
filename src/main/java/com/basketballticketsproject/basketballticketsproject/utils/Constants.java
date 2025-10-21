447a29234411ae943b01f3db24356dc3ef5b5022
Paul <paul-alexandru.stanescu@t-systems.com>
2025-10-21 15:35:32 +0200

    Corrección número entradas totales

--- src/main/java/com/basketballticketsproject/basketballticketsproject/utils/Constants.java
+++ src/main/java/com/basketballticketsproject/basketballticketsproject/utils/Constants.java
@@ -19,7 +19,7 @@ public class Constants {
 
     public static final String ENTRADAS_PATH = "../Entradas";
 
-    public static final int NUM_ENTRADAS = 50;
+    public static final int NUM_ENTRADAS = 10;
 
     public static String EMAIL_ASUNTO = "PARTIDOS GRANADA";
 
