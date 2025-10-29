3d49aca8fa525f7810629584226bdec81b843474
Paul <paul-alexandru.stanescu@t-systems.com>
2025-10-29 17:01:35 +0100

    Correcciones símbolos contraseña

--- src/main/java/com/basketballticketsproject/basketballticketsproject/utils/Constants.java
+++ src/main/java/com/basketballticketsproject/basketballticketsproject/utils/Constants.java
@@ -15,7 +15,7 @@ public class Constants {
     public static final String DATE_FORMATTER_CARPTETAS = "yyyy-MM-dd";
 
     //La contraseña tiene que tener: un numero, sin espacios, minuscula, mayuscula, un caracter epecial, y minimo 8 caracteres
-    public static final String PASSWORD_REGEX = "^(?=.*\\d)(?=\\S+$)(?=.*[@#$%^&+=*_-])(?=.*[a-z])(?=.*[A-Z]).{8,}$";
+    public static final String PASSWORD_REGEX = "^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?~`]).{8,}$";
 
     public static final String ENTRADAS_PATH = "../Entradas";
 
